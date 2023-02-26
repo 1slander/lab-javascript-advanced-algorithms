bced3f7ae31a9424286f7a9c1d05e6014aef53d7
1slander <hectorgomendez@gmail.com>
2023-02-26 13:21:51 +0000

    Finished Stack Part

--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -6,22 +6,38 @@ class Stack {
 
   canPush() {
     // ... your code goes here
+    return (this.stackControl.length === this.MAX_SIZE) ? false : true;
   }
 
   isEmpty() {
     // ... your code goes here
+    return (!this.stackControl.length) ? true:false;
   }
 
   push(item) {
     // ... your code goes here
+    const canPush=this.canPush();
+    if(canPush===true){
+      this.stackControl.push(item);
+      return this.stackControl;
+    }else{
+      throw new Error('STACK_OVERFLOW');
+    }
   }
 
   pop() {
     // ... your code goes here
+    const isEmpty=this.isEmpty();
+    if(isEmpty===false){
+     return this.stackControl.pop();
+    } else {
+      throw new Error('STACK_UNDERFLOW');
+    }
   }
 
   display() {
     // ... your code goes here
+    return this.stackControl
   }  
 }
 
